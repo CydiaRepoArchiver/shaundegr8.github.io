9e46abdc9a5418011785f9afe1624dbf5643b60b
shaundegr8 <shaundegr8@gmail.com>
2015-10-31 18:34:07 +0000

    added new package

--- packages.sh
+++ packages.sh
@@ -2,4 +2,5 @@
 dpkg-deb -bZgzip projects/LiveWallpaperPack1 debs
 dpkg-deb -bZgzip projects/JarvisForSiri debs
 dpkg-deb -bZgzip projects/OriginalSiriSounds debs
+dpkg-deb -bZgzip projects/Gotham8 debs
 # dpkg-deb -bZgzip projects/<Project Name> <Output Folder>
