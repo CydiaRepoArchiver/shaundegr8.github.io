f9b2d015cceffb295da3777b9469166bc6ac6ec5
shaundegr8 <shaundegr8@gmail.com>
2015-11-02 03:11:20 +0000

    added two packages

--- packages.sh
+++ packages.sh
@@ -3,4 +3,7 @@ dpkg-deb -bZgzip projects/LiveWallpaperPack1 debs
 dpkg-deb -bZgzip projects/JarvisForSiri debs
 dpkg-deb -bZgzip projects/OriginalSiriSounds debs
 dpkg-deb -bZgzip projects/Gotham8 debs
+dpkg-deb -bZgzip projects/AppleReversal debs
+dpkg-deb -bZgzip projects/AppleDefault6 debs
+
 # dpkg-deb -bZgzip projects/<Project Name> <Output Folder>
