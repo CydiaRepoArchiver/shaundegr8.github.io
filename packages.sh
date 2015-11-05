7815fba4f68f475bd10787bcc8fdfffad2a88e3e
shaundegr8 <shaundegr8@gmail.com>
2015-11-04 21:24:07 -0500

    updated and pushed a new package

--- packages.sh
+++ packages.sh
@@ -6,5 +6,6 @@ dpkg-deb -bZgzip projects/Gotham8 debs
 dpkg-deb -bZgzip projects/AppleReversal debs
 dpkg-deb -bZgzip projects/AppleDefault6 debs
 dpkg-deb -bZgzip projects/AppleOriginal6+ debs
+dpkg-deb -bZgzip projects/AppleReversal2 debs
 
 # dpkg-deb -bZgzip projects/<Project Name> <Output Folder>
