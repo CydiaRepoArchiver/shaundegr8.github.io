1a91ab3c559b8ce314dfbb3effb7f27e30a21c5a
shaundegr8 <shaundegr8@gmail.com>
2015-11-07 02:22:07 -0500

    tried using depictions

---
+++ com.shaundegr8.applereversal.php
@@ -0,0 +1,14 @@
+<?php
+    $name="PreferenceReseter";
+    $description=array(
+    "<text style=\"color: red;\">Apple Live Wallpapers with the Reversal effect for iPhone 6 and below! This package contains the fish! AppleReversal2 Contains the ink as all together they were too big to be pushed in one package :)</text>",
+    "Adds wallpapers to the live section of the iPhone!",
+    "If you'd like to design me an icon, please do so!");
+    
+    $changelog=array(
+    "2.2", " - Initial Release",
+    		  "Added the fish wallpapers!" );
+    
+    $devText="Developer's Twitter";
+    $devLink="https://twitter.com/shaundegr82";
+?>
---
+++ template/index/devLink.php
@@ -0,0 +1,4 @@
+<ul>
+    <li>
+        <a class=\"devLink\" target=\"_blank\" href="https://twitter.com/YourTwitter">@shaundegr82 on Twitter</a></li>
+    <li>
