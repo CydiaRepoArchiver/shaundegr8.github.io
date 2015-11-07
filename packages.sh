92c5f862c1ad5fd1f647d195dd5f0c041881f890
shaundegr8 <shaundegr8@gmail.com>
2015-11-07 01:55:35 -0500

    added two new packages

--- packages.sh
+++ packages.sh
@@ -7,5 +7,7 @@ dpkg-deb -bZgzip projects/AppleReversal debs
 dpkg-deb -bZgzip projects/AppleDefault6 debs
 dpkg-deb -bZgzip projects/AppleOriginal6+ debs
 dpkg-deb -bZgzip projects/AppleReversal2 debs
+dpkg-deb -bZgzip projects/AppleReversal6+ debs
+dpkg-deb -bZgzip projects/AppleReversal6+Part2 debs
 
 # dpkg-deb -bZgzip projects/<Project Name> <Output Folder>
